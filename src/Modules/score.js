d1016081278833aecd0c5326d0e33fa07f7075c2
Odette Gleen <nkargleeny@gmail.com>
2022-09-02 22:06:13 +0100

    used API to store the scores in a given game

--- src/Modules/score.js
+++ src/Modules/score.js
@@ -1,9 +1,26 @@
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
-export default class PIECE {
-  constructor(name, score) {
-    this.name = name;
-    this.score = score;
-    this.id = uuidv4();
-  }
+// export default class PIECE {
+//   constructor(name, score) {
+//     this.name = name;
+//     this.score = score;
+//     this.id = uuidv4();
+//   }
+// }
+// 'Game with ID: GrlP35gyONINxwNnhDtc added.'}
+
+async function userScores(a, b) {
+  const BaseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/GrlP35gyONINxwNnhDtc/scores';
+  fetch(BaseUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: a,
+      score: b,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json()).then((data) => data);
 }
+
+export default userScores;
